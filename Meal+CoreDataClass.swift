05354db3860e14ad3431a5030c4f9b82d582361d
DmitryP0l <dmitrypoletaeu@gmail.com>
2022-06-05 21:43:05 +0300

    commit final

---
+++ Meal+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by lion on 3.06.22.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
